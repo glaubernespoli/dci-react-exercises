389b349b64f714571b12e4417358531a165fea07
arifur-ra <ararif_017@yahoo.com>
2021-02-09 16:06:19 +0100

    main componet

--- api-pixabay-exercise/src/App.jsx
+++ api-pixabay-exercise/src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css';
+import Main from "./api_pixabay/Main"
+import "./App.css"
 
 function App() {
   return (
     <>
+      <Main />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
